9144d3f2beee15f0d56ab03bbe87f08434b0dddd
Augusto TMW <augustotmw@gmail.com>
2018-06-12 11:47:02 -0300

    tabs directive and tools factory

---
+++ angularjs/directive-tabs-tmw.js
@@ -0,0 +1,142 @@
+
+
+
+angular.module("MyApp",[]).factory("tmwTools", [function(){
+
+    var $tools = {
+        scrollTo: function(coordY, config){
+            config = angular.extend({}, {
+                adjust: 0,
+                callback: function(){ return true; },
+                menu: 0
+            },(config&&(config instanceof Object)?config:{}));
+
+            angular.element("html, body").animate({"scrollTop": (coordY + config.adjust) - config.menu}, this.animation.speed, function(){
+                config.callback(coordY, config, this);
+            });
+        }
+    }
+
+    return $tools;
+}]);
+
+/**@func tabsBlock
+ * @type AngularJS directive
+ * @requires AngularJS
+  * @desc 
+ * @version 0.0.1.2018041001
+ * 
+ */
+
+
+angular.module("MyApp",[]).directive("tabsBlock", ["$timeout","$rootScope","tmwTools", function($timeout, $rootScope, tmwTools){
+    return {
+        restrict: "A",
+        link: function(scope, element, attrs){
+
+            var triggers = element.find("[tab]");
+            var tabs = element.find(".tabs");
+            var panels = element.find("[tab-panel]");
+            var _winw = angular.element(window).outerWidth();
+            var elClasse = element.prop("class");
+
+            var _ctrl = element.parents("[ng-controller]");
+            var ctrlName = _ctrl.length ? _ctrl.attr("ng-controller") : "";
+            var tabName = "$tabsBlock";
+
+            element.find("[tab]").on("click", function(ev){
+                var $this = angular.element(this);
+                var tabnum =  $this.attr("tab");
+
+                if(!$this.hasClass("active")) {
+                    angular.element(".tab-item.active", element).removeClass("active");
+                    triggers.removeClass("active");
+                    panels.removeClass("active");
+                    triggers.filter("[tab="+tabnum+"]").addClass("active");
+                    var _panel = panels.filter(":eq("+tabnum+")");
+                    _panel.addClass("active");
+                    _panel.parents(".tab-item:first").addClass("active");
+
+                    element.prop("class", elClasse).addClass("tab-"+tabnum+"-selected");
+
+                    if(_winw < 768){
+                        tmwTools.scrollTo(_panel.parents(".tab-item:first").offset().top, {menu: true, adjust: -30});
+                    }
+
+                } else {
+                    if(_winw < 768){
+                        triggers.removeClass("active");
+                        panels.removeClass("active");
+                    }
+                }
+
+                try {
+                    attrs.onChangeTab && attrs.onChangeTab != "" ? eval("scope."+(attrs.onChangeTab).replace(/\([\w\W]{0,}/gi,"")+"(tabnum, this, ev)") : false;
+                } catch(err) {
+                    console.warn("Function \""+attrs.onChangeTab+"\" not found at controllers scope context.");
+                }
+
+
+            });
+
+            var _wInterval = false;
+
+            $(window).on("resize", function(){
+                
+                    if(_wInterval) {
+                        window.clearInterval(_wInterval);
+                    }
+    
+                    _wInterval = window.setInterval(function(){
+                        _winw = angular.element(window).outerWidth();
+                        if(_winw > 767 && !angular.element(".tab-item.active", element).length) {
+                            triggers.filter(":first").trigger("click");
+                        }
+                        window.clearInterval(_wInterval);
+                    },350);
+                
+            });
+
+            scope.$on("tabsBlock.set.tab", function(ev, tab){
+
+                if((tab || tab == 0) && (typeof tab == "number" || typeof tab == "string")) {
+                    var eltab = element.find("[tab="+tab+"]:not(.active):visible");
+                    if(eltab.length) {
+                        element.find("[tab="+tab+"]:visible").trigger("click");
+                    }
+                }
+
+            });
+
+            $timeout(function(){
+                if(_winw > 767) {
+                    var _flux = function(tabnum) {
+                        
+                        triggers.filter("[tab="+tabnum+"]").addClass("active");
+                        var _panel = panels.filter(":eq("+tabnum+")");
+                        _panel.addClass("active");
+                        _panel.parents(".tab-item:first").addClass("active");
+
+                        element.prop("class", elClasse).addClass("tab-"+tabnum+"-selected");
+                    }
+                    
+                    if(attrs.startAtTab && !isNaN(parseInt(attrs.startAtTab))) {
+                        _flux(parseInt(attrs.startAtTab));
+                        // triggers.filter(":eq("+attrs.startAtTab+")").trigger("click");
+                    } else {
+                        _flux(0);
+                        // triggers.filter(":first").trigger("click");
+                    }
+                }
+            });
+
+            try {
+                attrs.onLoadTabs && attrs.onLoadTabs != "" ? eval("scope."+(attrs.onLoadTabs).replace(/\([\w\W]{0,}/gi,"")+"(element, attrs)") : false;
+            } catch(err){
+                console.warn("Function \""+attrs.onLoadTabs+"\" not found at controllers scope context.");
+            }
+
+
+        }
+    }
+}]);
---
+++ angularjs/factory-tools-tmw.js
@@ -0,0 +1,56 @@
+angular.module("MyApp",[]).factory("tmwTools", [function(){
+
+    var $tools = {
+        animation: {
+            speed: 500
+        },
+        scrollTo: function(coordY, config){
+            config = angular.extend({}, {
+                adjust: 0,
+                callback: function(){ return true; },
+                menu: 0
+            },(config&&(config instanceof Object)?config:{}));
+
+            angular.element("html, body").animate({"scrollTop": (coordY + config.adjust) - config.menu}, this.animation.speed, function(){
+                config.callback(coordY, config, this);
+            });
+        },
+        regex: {
+            alphanum: (/[0-9a-zA-ZÀÁÂÃÄÅǍàáâãäåǎÈÉÊËèéêëĚěÊ̌ê̌Ɛ̌ɛ̌ÌÍÎÏìíîïǏǐÒÓÔÕÖØòóôõöøǑǒÙÚÛÜùúûüǓǔǙǚÝŸÿýY̌y̌ÇçÑñ\s\-]/g),
+            name: (/[^a-zA-ZÀÁÂÃÄÅǍàáâãäåǎÈÉÊËèéêëĚěÊ̌ê̌Ɛ̌ɛ̌ÌÍÎÏìíîïǏǐÒÓÔÕÖØòóôõöøǑǒÙÚÛÜùúûüǓǔǙǚÝŸÿýY̌y̌ÇçÑñ\s\-]/g),
+            accents: {
+                all: (/[ÀÁÂÃÄÅǍàáâãäåǎÈÉÊËèéêëĚěÊ̌ê̌Ɛ̌ɛ̌ÌÍÎÏìíîïǏǐÒÓÔÕÖØòóôõöøǑǒÙÚÛÜùúûüǓǔǙǚÝŸÿýY̌y̌ÇçÑñ]/g),
+                a: (/[ÀÁÂÃÄÅǍàáâãäåǎ]/g),
+                e: (/[ÈÉÊËèéêëĚěÊ̌ê̌Ɛ̌ɛ̌]/g),
+                i: (/[ÌÍÎÏìíîïǏǐ]/g),
+                o: (/[ÒÓÔÕÖØòóôõöøǑǒ]/g),
+                u: (/[ÙÚÛÜùúûüǓǔǙǚ]/g),
+                n: (/[Ññ]/g),
+                y: (/[ÝŸÿýY̌y̌]/g),
+                c: (/[Çç]/g)
+            },
+            stopwords: (/\b(de|a|o|que|e|do|da|em|um|para|é|com|não|uma|os|no|se|na|por|mais|as|dos|como|mas|foi|ao|ele|das|tem|à|seu|sua|ou|ser|quando|muito|há|nos|já|está|eu|também|só|pelo|pela|até|isso|ela|entre|era|depois|sem|mesmo|aos|ter|seus|quem|nas|me|esse|eles|estão|você|tinha|foram|essa|num|nem|suas|meu|às|minha|têm|numa|pelos|elas|havia|seja|qual|será|nós|tenho|lhe|deles|essas|esses|pelas|este|fosse|dele|tu|te|vocês|vos|lhes|meus|minhas|teu|tua|teus|tuas|nosso|nossa|nossos|nossas|dela|delas|esta|estes|estas|aquele|aquela|aqueles|aquelas|isto|aquilo|estou|está|estamos|estão|estive|esteve|estivemos|estiveram|estava|estávamos|estavam|estivera|estivéramos|esteja|estejamos|estejam|estivesse|estivéssemos|estivessem|estiver|estivermos|estiverem|hei|há|havemos|hão|houve|houvemos|houveram|houvera|houvéramos|haja|hajamos|hajam|houvesse|houvéssemos|houvessem|houver|houvermos|houverem|houverei|houverá|houveremos|houverão|houveria|houveríamos|houveriam|sou|somos|são|era|éramos|eram|fui|foi|fomos|foram|fora|fôramos|seja|sejamos|sejam|fosse|fôssemos|fossem|for|formos|forem|serei|será|seremos|serão|seria|seríamos|seriam|tenho|tem|temos|tém|tinha|tínhamos|tinham|tive|teve|tivemos|tiveram|tivera|tivéramos|tenha|tenhamos|tenham|tivesse|tivéssemos|tivessem|tiver|tivermos|tiverem|terei|terá|teremos|terão|teria|teríamos|teriam)\b/g)
+        },
+        remove: {
+            accents: function(letters, term){
+                if(letters && typeof letters == "string" && letters != "") {
+                    letters = letters.replace(/[\W\d]/gi, "").split("");
+                    for(var i=0; i<letters.length; i++) {
+                        // console.log(letters[i]);
+                        if($tools.regex.accents[letters[i]]) {
+                            term = term.replace($tools.regex.accents[letters[i]], letters[i]);
+                        }
+                    }
+                    return term;
+                } else {
+                    return term;
+                }
+            },
+            stopwords: function(term){
+                return term.replace($tools.regex.stopwords, "");
+            }
+        }
+    }
+
+    return $tools;
+}]);
