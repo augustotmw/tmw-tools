74702cbb0b8e5ee149a6a7ec67b9f5de30838808
Augusto TMW <augustotmw@gmail.com>
2018-06-20 13:08:03 -0300

    upgrade factory tools

--- angularjs/factory-tools-tmw.js
+++ angularjs/factory-tools-tmw.js
@@ -1,8 +1,25 @@
-angular.module("MyApp",[]).factory("tmwTools", [function(){
+angular.module("MyApp",[]).factory("tmwTools", ["$window",function($window){
 
     var $tools = {
         animation: {
             speed: 500
+        },
+        localStorage: {
+            delete: function(key){
+                $window.localStorage.removeItem(key);
+            },
+            set: function (key, value) {
+                $window.localStorage[key] = value;
+            },
+            get: function (key, defaultValue) {
+                return $window.localStorage[key] || defaultValue;
+            },
+            setObject: function (key, value) {
+                $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function (key) {
+                return JSON.parse($window.localStorage[key] || '{}');
+            }
         },
         scrollTo: function(coordY, config){
             config = angular.extend({}, {
